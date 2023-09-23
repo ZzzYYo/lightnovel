2fe7c313a8388ff7f88fc15b3d5b95daa41408f7
ZzzYYo <134829700+ZzzYYo@users.noreply.github.com>
2023-09-23 14:19:44 +0800

    提交初始项目

---
+++ src/main/java/com/zzzyyo/lightnovel/LightNovelApplication.java
@@ -0,0 +1,13 @@
+package com.zzzyyo.lightnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightNovelApplication.class, args);
+    }
+
+}
